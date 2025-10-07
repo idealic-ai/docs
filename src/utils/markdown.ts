6e4beb24f7a10eb6c76f15d9d1aa61ffc6bf8cf8
Yaroslaff Fedin <invizko@MacBook-Pro-khxngYaroslaff.local>
2025-10-07 12:51:19 +0800

    Add edict

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -1,10 +1,13 @@
 import { marked } from 'marked';
+import markedFootnote from 'marked-footnote';
+
+marked.use(markedFootnote());
 
 export async function processMarkdown(markdownContent: string): Promise<string> {
   let htmlContent = await Promise.resolve(marked.parse(markdownContent));
 
   // Wrap tables in figure tags
-  htmlContent = htmlContent.replace(/<table>/g, '<figure><table>');
+  htmlContent = htmlContent.replace(/<table>/g, '<figure class="fullwidth"><table>');
   htmlContent = htmlContent.replace(/<\/table>/g, '</table></figure>');
 
   return htmlContent;
