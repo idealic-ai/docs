fd0945f10c33dcd2a02b07ae2ad10d0625cc0b33
Yaroslaff Fedin <invizko@MacBook-Pro-khxngYaroslaff.local>
2025-10-14 08:57:04 +0800

    Parametrize base path

--- src/+config.ts
+++ src/+config.ts
@@ -10,7 +10,7 @@ export default {
   Layout,
 
   redirects: {
-    '/': `${process.env.VITE_BASE_PATH}/en/`,
+    '/': `${process.env.VITE_BASE_PATH || ''}/en/`,
   },
 
   // https://vike.dev/head-tags
